5a8a96877f41046cab4a90888f15a266fa58d7ed
jimmy <chioulf2@yahoo.com.tw>
2015-06-09 16:44:40 +0800

    可以存文章內文跟字數

--- get_api.py
+++ get_api.py
@@ -16,7 +16,17 @@ import get_blog_detail as gbd
 
 #存csv
 f = open("article.csv","w") 
-# print(decodejson['articles'])
+#標頭
+f.write('標題' + ',')
+f.write('人氣' + ',')
+f.write('作者' + ',')
+f.write('照片數' + ',')
+f.write('內文' + ',')
+f.write('內文字數' + ',')
+f.write('Tag' + ',')
+f.write('\n')
+
+
 for i in decodejson['articles']:
 	if(str(i['site_category_id']) == '26'):	#芸芸限定 美味食記
 		# print('success')
@@ -27,6 +37,8 @@ for i in decodejson['articles']:
 	    blog_info = gbd.get_blog_info(i['link'].encode('UTF-8'), i['id'], i['user']['name'])
 	    print(blog_info)
 	    f.write(str(blog_info['photo_count']) + ',')
+	    f.write(str(blog_info['content']) + ',')
+	    f.write(str(blog_info['content_count']) + ',')
 	    #讀取tag 並串接成一個欄位
 	    final_tag = '' #串接所有tag用字串
 	    for tag in i['tags']:
--- get_blog_detail.py
+++ get_blog_detail.py
@@ -4,21 +4,29 @@ import urllib2
 import json
 
 import time
+import re
 
 #爬蟲 回傳dict 包含 圖片數 文字數 讚數
 def get_blog_info(site, article_id, user):
-	article_dict = {}
-	url = 'https://emma.pixnet.cc/blog/articles/'+str(article_id)+'?user='+user+'&format=json'
-
+	article_dict = {} #存回傳結果
+	
+	#用來爬文章詳細api的
+	api_url = 'https://emma.pixnet.cc/blog/articles/'+str(article_id)+'?user='+user+'&format=json'
+	print(api_url)
 	#先抓照片數
-	response = urllib2.urlopen(url)
+	response = urllib2.urlopen(api_url)
 	response_json = response.read()
-	# print(response_json)
 	decode_json = json.loads(response_json)
+
 	article_dict['photo_count'] = len(decode_json['article']['images'])
-	# print(decode_json)
-	# body = decode_json['body'].encode('UTF-8')
-	# print(body)
+	body = decode_json['article']['body'].encode('UTF-8')
+	#regular expression
+	p = re.compile('(<.*?>|&nbsp;|&darr;|<!--.*?-->)')	
+	content = p.sub('',body)
+	article_dict['content'] = content.replace(",","，")
+	article_dict['content_count'] = len(content)
+	# p = re.compile( '(one|two|three)') 
+ # 	print(p.sub( 'num', 'one word two words three words'))
 
 	#再來用網址去爬 article-content裡面用正規表達拿文字數  然後拿FB讚數 然後用beautysoup
 
