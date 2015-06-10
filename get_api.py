388fc09bc50fe99672bebde572d5abff7190b3e0
jimmy <chioulf2@yahoo.com.tw>
2015-06-10 12:21:50 +0800

    加入一些例外處理&不同情況判斷 抓好3個人的

--- get_api.py
+++ get_api.py
@@ -3,55 +3,74 @@
 import urllib2
 import json
 
-#愛吃鬼云云食記
-response = urllib2.urlopen("https://emma.pixnet.cc/blog/articles?per_page=10&user=anise&format=json")
-response_json = response.read()
-decodejson = json.loads(response_json)
-
-
 import csv
+import time
 import get_blog_detail as gbd
 
+#作者清單
+#'anise', 'bluehero', 'carriewang', 
+user_list = ['jesse0218', 'meijuily',
+			 'meiko1101', 'millychun', 'nellydyu', 'paine0602', 'sezna627']
 
 
-#存csv
-f = open("article.csv","w") 
-#標頭
-f.write('標題' + ',')
-f.write('人氣' + ',')
-f.write('讚數' + ',')
-f.write('轉換率(讚數/人氣)' + ',')
-f.write('作者' + ',')
-f.write('照片數' + ',')
-f.write('內文' + ',')
-f.write('內文字數' + ',')
-f.write('Tag' + ',')
-f.write('\n')
 
 
-for i in decodejson['articles']:
-	if(str(i['site_category_id']) == '26'):	#芸芸限定 美味食記
-		#獲得圖片數 字數 讚數
-	    blog_info = gbd.get_blog_info(i['link'].encode('UTF-8'), i['id'], i['user']['name'])
-	    
-	    f.write(i['title'].encode('UTF-8') + ',')
-	    f.write(str(i['hits']['total']) + ',')
-	    f.write(str(blog_info['fb_likes']) + ',')
-	    f.write(str(float(blog_info['fb_likes'])/float(i['hits']['total'])) + ',')
-	    f.write(i['user']['name'] + ',')
-	    # print(blog_info)
-	    f.write(str(blog_info['photo_count']) + ',')
-	    f.write(str(blog_info['content']) + ',')
-	    f.write(str(blog_info['content_count']) + ',')
-	    #讀取tag 並串接成一個欄位
-	    final_tag = '' #串接所有tag用字串
-	    for tag in i['tags']:
-	    	final_tag += (tag['tag'].encode('UTF-8') + '/')
-	    f.write(final_tag + ',')	
+for user in user_list:
+	#存csv 一個人一個
+	csv_title = str(user)+'.csv'
+	f = open(csv_title,"w") 
 
-	    f.write('\n')
+	api_url = "https://emma.pixnet.cc/blog/articles?user="+user+"&format=json"
+	print('loading '+api_url)
+	response = urllib2.urlopen(api_url)
+	response_json = response.read()
+	decodejson = json.loads(response_json)
 
-f.close()
+	#標頭
+	f.write('標題' + ',')
+	f.write('人氣' + ',')
+	f.write('讚數' + ',')
+	f.write('轉換率(讚數/人氣)' + ',')
+	f.write('作者' + ',')
+	f.write('作者個人分類(每個人不同)' + ',')
+	f.write('照片數' + ',')
+	f.write('內文' + ',')
+	f.write('內文字數' + ',')
+	f.write('Tag' + ',')
+	f.write('\n')
+
+	for i in decodejson['articles']:
+		if(str(i['site_category_id']) == '26'):	#限定"美味食記"分類
+			try:
+				#獲得圖片數 字數 讚數
+			    blog_info = gbd.get_blog_info(i['link'].encode('UTF-8'), i['id'], i['user']['name'])
+			    if blog_info['fb_likes'] != '':
+				    f.write(i['title'].encode('UTF-8') + ',')
+				    f.write(str(i['hits']['total']) + ',')
+				    f.write(str(blog_info['fb_likes']) + ',')
+				    f.write(str(float(blog_info['fb_likes'])/float(i['hits']['total'])) + ',')
+				    f.write(i['user']['name'] + ',')
+				    f.write(i['category'].encode('UTF-8') + ',')
+				    f.write(str(blog_info['photo_count']) + ',')
+				    f.write(str(blog_info['content']) + ',')
+				    f.write(str(blog_info['content_count']) + ',')
+				    #讀取tag 並串接成一個欄位
+				    final_tag = '' #串接所有tag用字串
+				    for tag in i['tags']:
+				    	final_tag += (tag['tag'].encode('UTF-8') + '/')
+				    f.write(final_tag + ',')	
+
+				    f.write('\n')
+				    print("success!")
+			except:
+				print("error occur...")
+			#休息一下
+			time.sleep(0.5)
+
+	print(user+" info done!")
+	f.close()
+	time.sleep(3)
+
 
 
 
--- get_blog_detail.py
+++ get_blog_detail.py
@@ -3,46 +3,59 @@
 import urllib2
 import json
 
-import time
 import re
+import time
 
 #爬蟲 回傳dict 包含 圖片數 文字數 讚數
 def get_blog_info(site, article_id, user):
 	article_dict = {} #存回傳結果
 
-	#用來爬文章詳細api的
-	api_url = 'https://emma.pixnet.cc/blog/articles/'+str(article_id)+'?user='+user+'&format=json'
-	print(api_url)
-	#先抓照片數
-	response = urllib2.urlopen(api_url)
-	response_json = response.read()
-	decode_json = json.loads(response_json)
+	try:
+		#用來爬文章詳細api的
+		api_url = 'https://emma.pixnet.cc/blog/articles/'+str(article_id)+'?user='+user+'&format=json'
+		print('loading '+api_url)
+		#先抓照片數
+		response = urllib2.urlopen(api_url)
+		response_json = response.read()
+		decode_json = json.loads(response_json)
 
-	article_dict['photo_count'] = len(decode_json['article']['images'])
-	#用正規表達去除tag 拿文字數 (跟html的article-content裡面一樣的東西)
-	body = decode_json['article']['body'].encode('UTF-8')
-	p = re.compile('(<.*?>|&nbsp;|&darr;|<!--.*?-->)')	
-	content = p.sub('',body)
-	article_dict['content'] = content.replace(",","，")
-	article_dict['content_count'] = len(content)
-	# p = re.compile( '(one|two|three)') 
- 	# print(p.sub( 'num', 'one word two words three words'))
+		article_dict['photo_count'] = len(decode_json['article']['images'])
+		#用正規表達去除tag 拿文字數 (跟html的article-content裡面一樣的東西)
+		body = decode_json['article']['body'].encode('UTF-8')
+		p = re.compile('(<.*?>|&nbsp;|&darr;|<!--.*?-->)')	
+		content = p.sub('',body)
+		article_dict['content'] = content.replace(",","，")
+		article_dict['content_count'] = len(content)
+		# p = re.compile( '(one|two|three)') 
+	 	# print(p.sub( 'num', 'one word two words three words'))
+ 	except:
+		print("error occur in situation2...")
 
 	#再來用網址去爬 用beautyfulsoup拿FB讚數 
 	from bs4 import BeautifulSoup
 
-	#fb plugin的網址 發現的規則
-	fb_like_url = 'https://www.facebook.com/plugins/like.php?href='+site+'&show_faces=false&width=500&action=like&font=verdana&colorscheme=light&height=30'
-	response = urllib2.urlopen(fb_like_url)
-	response_html = response.read()
+	try:
+		#休息一下
+		time.sleep(0.5)
 
-	soup = BeautifulSoup(''.join(response_html))
-	fb_like = soup.findAll("span", { "class" : "hidden_elem" })
-	for like in fb_like:
-		p = re.compile('[^0-9]') #將那段文字去除到只剩數字
-		content = p.sub('',like.get_text())
-		print(content)
-		article_dict['fb_likes'] = content
+		#fb plugin的網址 發現的規則
+		article_dict['fb_likes'] = '' #先宣告出來 以利前面判斷
+
+		fb_like_url = 'http://www.facebook.com/plugins/like.php?href='+site
+		print('loading fb_like_url...')
+		response = urllib2.urlopen(fb_like_url) 
+		response_html = response.read()
+
+		soup = BeautifulSoup(''.join(response_html))
+		fb_like = soup.findAll("span", { "class" : "hidden_elem" })
+		for like in fb_like:
+			p = re.compile('[^0-9]') #將那段文字去除到只剩數字
+			content = p.sub('',like.get_text())
+			# print(content)
+			article_dict['fb_likes'] = content
+	except:
+		# print("get fb like timeout")
+		print("error occur in situation3...")
 
 
 	# #用ghost.py
@@ -55,5 +68,4 @@ def get_blog_info(site, article_id, user):
 	# # ghost.show()
 	# # ghost.sleep(2)
 
-	time.sleep(0.3)
 	return article_dict
